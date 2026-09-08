3e20049cfcc1fd62658e9bf2057144548087214c
mntzrh609-creator <mntzrh609@gmail.com>
2026-09-08 20:53:08 +0300

    Main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import os
+import requests
 
+TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHANNEL = "@MH999R"
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+data = {
+    "chat_id": CHANNEL,
+    "text": "🔴 اختبار نظام الأخبار — إذا وصلت هذه الرسالة فالبوت يعمل بشكل صحيح."
+}
+
+response = requests.post(url, data=data, timeout=30)
+
+print("Status:", response.status_code)
+print("Response:", response.text)
